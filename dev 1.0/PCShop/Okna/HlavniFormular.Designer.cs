610c8f1dbce3f1b3bd9447e7c39c31636e2afce8
vala03 <vala03@student.vspj.cz>
2016-11-06 22:46:29 +0100

    Druhá verze program

---
+++ dev 1.0/PCShop/Okna/HlavniFormular.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace PCShop
+{
+    partial class HlavniFormular
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.programToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ukončitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.programToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(758, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // programToolStripMenuItem
+            // 
+            this.programToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ukončitToolStripMenuItem});
+            this.programToolStripMenuItem.Name = "programToolStripMenuItem";
+            this.programToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.programToolStripMenuItem.Text = "Program";
+            // 
+            // ukončitToolStripMenuItem
+            // 
+            this.ukončitToolStripMenuItem.Name = "ukončitToolStripMenuItem";
+            this.ukončitToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.ukončitToolStripMenuItem.Text = "Ukončit";
+            this.ukončitToolStripMenuItem.Click += new System.EventHandler(this.ukončitToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(240, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(506, 270);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 309);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PC-Shop evidence";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem programToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ukončitToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    }
+}
+
---
+++ dev 1.0/PCShop/Tridy/Databaze.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Reflection;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace PCShop
+{
+    class Databaze
+    {
+        private SqlConnection dbPripojeni;
+        private SqlCommand dbPrikaz;
+        //private SqlDataReader reader;
+        private string ConnectionString;
+
+        public Databaze()
+        {
+            string exec = Assembly.GetExecutingAssembly().Location;
+            string cesta = (Path.GetDirectoryName(exec));
+            AppDomain.CurrentDomain.SetData("DataDirectory", cesta);
+            ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\databaze.mdf;Integrated Security=True;Connect Timeout=30";
+            dbPripojeni = new SqlConnection(ConnectionString);
+            dbPripojeni.Open();
+        }
+
+        public bool zavritDB
+        {
+            get
+            {
+                try
+                {
+                    dbPripojeni.Close();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        public int ZkontrolujPrihlaseni(string login, string heslo) //vystupy: id uzivatele=udaje ok, -1=spatne udaje
+        {
+            string passwd = md5_encrypt(heslo);
+            dbPrikaz = new SqlCommand("SELECT COUNT(*) FROM app_login WHERE login=@Login AND password=@Passwd", dbPripojeni);
+            dbPrikaz.Parameters.Add("@Login", System.Data.SqlDbType.NVarChar).Value = login;
+            dbPrikaz.Parameters.Add("@Passwd", System.Data.SqlDbType.NVarChar).Value = passwd;
+            if((int)dbPrikaz.ExecuteScalar() == 1)
+            {
+                dbPrikaz = new SqlCommand("SELECT id FROM app_login WHERE login=@Login AND password=@Passwd", dbPripojeni);
+                dbPrikaz.Parameters.Add("@Login", System.Data.SqlDbType.NVarChar).Value = login;
+                dbPrikaz.Parameters.Add("@Passwd", System.Data.SqlDbType.NVarChar).Value = passwd;
+                return (int)dbPrikaz.ExecuteScalar();
+            }
+            else
+                return -1;
+        }
+
+        /* private string md5_decrypt(string text)
+         {
+             if ((text == null) || (text.Length == 0))
+                 return string.Empty;
+
+             //Calculate MD5 hash. This requires that the string is splitted into a byte[].
+             MD5 md5 = new MD5CryptoServiceProvider();
+             byte[] textToHash = Encoding.Default.GetBytes(text);
+             byte[] vysledek = md5.ComputeHash(textToHash);
+
+             //konverze bitoveho pole zpatky na string
+             return BitConverter.ToString(vysledek);
+         }*/
+
+        private string md5_encrypt(string text)
+        {
+            MD5 md5Hash = MD5.Create();
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(text));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+    }
+}
